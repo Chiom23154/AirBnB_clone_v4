7a2c1278a972c1deec4488cf98c727cebde0a673
root <cephaspeter37@gmail.com>
2023-05-07 21:06:20 +0000

    replaced route 4-hbnb to 100-hbnb

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -1,48 +1,48 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """
-This is a flask app that integrates with airbnbn static html template.
+Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid
 
 
-# setup for flask
+# flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# start rendering flask page
+# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    this method calls .close() on the current SQLAlchemy session after each request
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
     """
     storage.close()
 
 
-@app.route('/0-hbnb')
+@app.route('/4-hbnb')
 def hbnb_filters(the_id=None):
     """
-    handles requests to custom template with, cities and amenities
+    handles request to custom template with states, cities & amentities
     """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-            for user in storage.all('User').values())
-    return render_template('0-hbnb.html', 
-            states=states, 
-            amens=amens, 
-            places=places, 
-            users=users, 
-            cache_id=uuid.uuid4())
+                 for user in storage.all('User').values())
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users,
+                           cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
     """
-    Main flask app
-    """
+    MAIN Flask App"""
     app.run(host=host, port=port)
